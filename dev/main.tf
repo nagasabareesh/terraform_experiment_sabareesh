ea0a7606e089aed563568cbde37375859400bea3
Naga G <sabarishgn@gmail.com>
2021-11-23 20:27:00 +0530

    Added modules and git ignore

--- dev/main.tf
+++ dev/main.tf
@@ -4,20 +4,21 @@ provider "aws" {
 }
 
 module "my_vpc" {
-    # source = "../modules/vpc"
-    source = "github.com/nagasabareesh/simpleterraform?ref=main"
-    vpc_cidr = "192.168.0.0/16"
-    tenacy = "default"
-    vpc_id = "${module.my_vpc.vpc_id}"
-    subnet_cidr = "192.168.1.0/24"
-    
+  # source = "../modules/vpc"
+  # source      = "github.com/nagasabareesh/simpleterraform?ref=main"
+  source      = "../modules/vpc"
+  vpc_cidr    = "192.168.0.0/16"
+  tenacy      = "default"
+  vpc_id      = "${module.my_vpc.vpc_id}"
+  subnet_cidr = "192.168.1.0/24"
+
 }
 
-module "my_ec2"{
-    source= "../modules/ec2"
-    ec2_count = 2
-    ami_id = "ami-03f4c416f489586a3"
-    subnet_id = "${module.my_vpc.subnet_id}"
+module "my_ec2" {
+  source    = "../modules/ec2"
+  ec2_count = 2
+  ami_id    = "ami-04902260ca3d33422"
+  subnet_id = "${module.my_vpc.subnet_id}"
 }
 
 resource "aws_security_group" "allow_tls" {
@@ -25,14 +26,15 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
   vpc_id      = "${module.my_vpc.vpc_id}"
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
+  # ingress {
+  #   description = "TLS from VPC"
+  #   from_port   = 443
+  #   to_port     = 443
+  #   protocol    = "tcp"
+  #   # cidr_blocks      = [aws_vpc.main.cidr_block]
+  #   cidr_blocks      = [module.my_vpc.cidr_block]
+  #   ipv6_cidr_blocks = [module.my_vpc.ipv6_cidr_block]
+  # }
 
   egress {
     from_port        = 0
