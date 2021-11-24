a88ff6ab21a8e43ac8dd2671bc92894e692bebd5
Naga G <sabarishgn@gmail.com>
2021-11-24 19:36:58 +0530

    Added files

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,13 +1,13 @@
 variable "ec2_count" {
-    default = 2
+  default = 2
 }
 
-variable "ami_id"{
-    default = "ami-03f4c416f489586a3"
+variable "ami_id" {
+  default = "ami-03f4c416f489586a3"
 }
 
-variable "instance_type"{
-    default = "t2.micro"
+variable "instance_type" {
+  default = "t2.micro"
 }
 
-variable "subnet_id"{}
+variable "subnet_id" {}
